6c4870ac0914aa0c227bcbd61195415fe1242a7a
agheri <43124733+agheri@users.noreply.github.com>
2021-07-02 19:47:58 -0400

    Add files via upload

---
+++ lab05/grades/lib/grades.ex
@@ -0,0 +1,21 @@
+defmodule Grades do
+  @moduledoc """
+  Grades keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+  #def percentage_grade(marks) do
+    #:rand.uniform(100)
+  #end
+
+  #def letter_grade(marks) do
+    #for _ <- 1..1, into: "", do: <<Enum.random('ABCDEF')>>
+  #end
+
+  #def numeric_grade(marks) do
+    #:rand.uniform(10)
+  #end
+
+end
